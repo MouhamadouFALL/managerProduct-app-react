72a90cbcad37dd147e04d19fd2ef726d0aaec57a
MouhamadouFALL <mouhamadou2.fall@gmail.com>
2021-12-28 13:11:18 +0000

    ajout des composants Header, Footer, Banner et le Container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
+import Banner from "./layout/Banner";
+import Container from "./Container";
+
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Banner />
+      <Switch>
+      <Route exact path="/">
+        <Container nom="Boom"/>
+      </Route>
+      {/*<Route path="/addProduct" component={AddProduct} />
+      <Route path="/editProduct" component={Editproduct} />*/}
+      </Switch>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
