6e815dd2af8d3f7ec0f3d4d25796b34fe2e9a4a9
MouhamadouFALL <mouhamadou2.fall@gmail.com>
2021-12-28 13:20:57 +0000

    ajout de la base de donnee products

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 
 class Productlist extends Component {
+
+    state = {isLoading: true, students : [], error: null }
+
+    // Récupérer tous les produits dans la base products
+    componentDidMount() {
+        axios.get('http://localhost:3003/learners')
+        .then( res => {
+            const students = res.data;
+            this.setState({ students, isLoading: false });
+        })
+        .catch( error => this.setState({ error, isLoading: false}))
+    }
+
+
     render() {
         return (
             <div>
