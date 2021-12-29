3c37a898fc5370ee46843c5aafd70d7099315467
MouhamadouFALL <mouhamadou2.fall@gmail.com>
2021-12-29 18:24:13 +0000

    Modification de la fonction handleChange de AddProduct.js

--- src/AddProduct.js
+++ src/AddProduct.js
@@ -6,15 +6,18 @@ class AddProduct extends Component {
 
     constructor() {
         super();
-        this.state = {nom: "", price: "", court_description: "", long_description: ""}
+        this.state = {nom: "", price: "", court_description: "", long_description: ""};
+
+        this.handleChange = this.handleChange.bind(this);
+        //this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     // Écouter les événement
-    //handleChange = even => this.setState({ [even.target.name]: even.target.value});
-    handleChangeNom = e => this.setState({ nom: e.target.value});
-    handleChangePrice = e => this.setState({ price: e.target.value});
-    handleChangeCourtDesc = e => this.setState({ court_description: e.target.value});
-    handleChangeLongDesc = e => this.setState({ long_description: e.target.value});
+    handleChange = e => this.setState({ [e.target.name]: e.target.value});
+    //handleChangeNom = e => this.setState({ nom: e.target.value});
+    //handleChangePrice = e => this.setState({ price: e.target.value});
+    //handleChangeCourtDesc = e => this.setState({ court_description: e.target.value});
+    //handleChangeLongDesc = e => this.setState({ long_description: e.target.value});
 
     // Ajouter le input du formulaire dans le state
     handleAdd = e => {
@@ -55,22 +58,22 @@ class AddProduct extends Component {
                     
                     <div className="mb-3">
                         <label htmlFor="Nom Produit" className="form-label">Nom Produit</label>
-                        <input type="text" name='nom' value={this.state.nom} onChange={this.handleChangeNom} className="form-control" placeholder="Entrer le nom du produit" />
+                        <input type="text" name='nom' value={this.state.nom} onChange={this.handleChange} className="form-control" placeholder="Entrer le nom du produit" />
                     </div>
 
                     <div className="mb-3">
                         <label htmlFor="Prix Produit" className="form-label">Price Produit</label>
-                        <input type="text" name='price' value={this.state.price} onChange={this.handleChangePrice} className="form-control" placeholder="Entrer le prix du produit" />
+                        <input type="text" name='price' value={this.state.price} onChange={this.handleChange} className="form-control" placeholder="Entrer le prix du produit" />
                     </div>
 
                     <div className="mb-3">
                         <label htmlFor="Description courte" className="form-label">Courte Description</label>
-                        <input type="text" name='court_des' value={this.state.court_description} onChange={this.handleChangeCourtDesc} className="form-control" placeholder="Entrer une courte description du produit" />
+                        <input type="text" name='court_description' value={this.state.court_description} onChange={this.handleChange} className="form-control" placeholder="Entrer une courte description du produit" />
                     </div>
 
                     <div className="mb-3">
                         <label htmlFor="Description longue" className="form-label">Description du Produit</label>
-                        <textarea name="long_des" value={this.state.long_description} onChange={this.handleChangeLongDesc} className="form-control" rows="2" placeholder='ici les details du produit' ></textarea>
+                        <textarea name="long_description" value={this.state.long_description} onChange={this.handleChange} className="form-control" rows="2" placeholder='ici les details du produit' ></textarea>
                     </div>
 
                     <div className="col-12">
